f2bb377ee78657828fa2cfe95ea90ef0a749eb04
Adebayo Ismail <adebayoismail377@gmail.com>
2025-12-17 19:33:34 +0100

    python accessing mysqlserver

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,34 +1,20 @@
-#!/usr/bin/python3
-"""
-Creates the database alx_book_store if it does not already exist
-"""
-
 import mysql.connector
-from mysql.connector import Error
 
-def create_database():
-    connection = None
-    try:
-        # Connect to MySQL server
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",        # change if needed
-            password="Luminousphotography;1 "         # change if needed
-        )
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Luminousphotography;1"
+    )
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
 
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-            print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as e:
+    print(f"Error while connecting to MySQL: {e}")
 
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
-
-    finally:
-        # Close the connection
-        if connection and connection.is_connected():
-            cursor.close()
-            connection.close()
-
-if __name__ == "__main__":
-    create_database()
+finally:
+    if connection and connection.is_connected():
+        cursor.close()
+        connection.close()
